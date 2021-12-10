c961a069d814e7de754002166f05739c45070d97
fediushkin <fediushkin@fim.uni-passau.de>
2021-12-10 16:31:04 +0100

    serialization done

--- Files_Docs_Serialization/src/Main.java
+++ Files_Docs_Serialization/src/Main.java
@@ -1,14 +1,19 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
+
+    private static int counter = 1;
+
     public static void main(String[] args) {
+
 
         StringBuilder logs = new StringBuilder();
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -52,7 +57,7 @@ public class Main {
 
         List<String> mainFiles = Arrays.asList("Main.java", "Utils.java");
 
-        for (String fileName: mainFiles) {
+        for (String fileName : mainFiles) {
             File myFile = new File(root + "src/main/" + fileName);
             try {
                 if (myFile.createNewFile())
@@ -65,13 +70,87 @@ public class Main {
         // Saving logs into temp.txt file
         //==============================================================
 
-        try (FileWriter writer = new FileWriter(root + "temp/temp.txt", true))
-        {
+        try (FileWriter writer = new FileWriter(root + "temp/temp.txt", true)) {
             writer.write(logs.toString());
             writer.flush();
         } catch (IOException ex) {
             logs.append(ex.getMessage());
         }
-        System.out.println(logs.toString());
+        //System.out.println(logs.toString());
+
+
+        // serialization of objects
+        String pathToSaveProgress = "/Users/Armi/IdeaProjects/Game/savegames/";
+
+        // instantiating object, which we are going to save and zip later
+        GameProgress milestone1 = new GameProgress(86, 1, 3, 42);
+        GameProgress milestone2 = new GameProgress(13, 7, 94, 99);
+        GameProgress milestone3 = new GameProgress(100, 10, 12, 55);
+
+        // saving (serializing object) game progress
+        saveGame(pathToSaveProgress, milestone1);
+        saveGame(pathToSaveProgress, milestone2);
+        saveGame(pathToSaveProgress, milestone3);
+
+        // Create a list of files to zip later
+        File dir = new File(pathToSaveProgress);
+        List<String> filesToZip = new ArrayList<>();
+
+        if (dir.isDirectory()) {
+
+            for (File item : dir.listFiles()) {
+
+                if (item.isDirectory()) {
+                    //System.out.println(item.getName() + " - каталог");
+                } else {
+                    //System.out.println(item.getName() + " - файл");
+                    filesToZip.add(pathToSaveProgress + item.getName());
+                }
+            }
+        }
+
+        // Calling zipping process. It ends with deleting files, which were packed into archive
+        zipFiles(pathToSaveProgress, filesToZip);
     }
+
+    // method to save (serialize objects) game progress
+    public static void saveGame(String path, GameProgress progress) {
+        try (
+                ObjectOutputStream progressSavior =
+                        new ObjectOutputStream(new FileOutputStream(path  + "data" + counter + ".data"));
+        ) {
+            progressSavior.writeObject(progress);
+            counter++;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void zipFiles(String path, List<String> stuffToPack) {
+        for (String save : stuffToPack) {
+            try (
+                    ZipOutputStream zippingProcess =
+                            new ZipOutputStream(new FileOutputStream(path + "savedGames.zip"));
+                    FileInputStream fileToPack = new FileInputStream(save);
+            ) {
+
+                zippingProcess.putNextEntry(new ZipEntry(save));
+
+                byte[] buffer  = new byte[fileToPack.available()];
+                fileToPack.read(buffer);
+
+                zippingProcess.write(buffer);
+                zippingProcess.closeEntry();
+
+                // once file is zipped we delete it from the folder
+                File fileToDelete = new File(save);
+                boolean success = fileToDelete.delete();
+                System.out.println(save + " deleting process -> "+ success);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
